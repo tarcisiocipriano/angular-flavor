0b72d9a929cff61f8606a095639cb721808db387
tarcisio cipriano <creativecipriano@gmail.com>
2020-03-16 15:32:27 -0300

    add the shopping list and shopping edit components

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.scss']
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false }) nameInput: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInput: ElementRef;
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() { }
+
+  onAddItem() {
+    const ingName = this.nameInput.nativeElement.value;
+    const ingAmount = this.amountInput.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
+  }
+
+}
