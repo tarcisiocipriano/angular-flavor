61275d526a3477581721ecc1702e8fca007d0d03
tarcisio cipriano <creativecipriano@gmail.com>
2020-03-16 14:51:51 -0300

    add the recipe and the shopping-list services

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Ingredient } from '../shared/ingredient.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingListService {
+  ingredientsChanged = new Subject<Ingredient[]>();
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  constructor() { }
+
+  getIngredients() {
+    return [...this.ingredients];
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next([...this.ingredients]);
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next([...this.ingredients]);
+  }
+
+}
