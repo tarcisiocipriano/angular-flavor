c4a417df4c30042d86aafb0704df58f4137d6781
Tarcisio Cipriano <creativecipriano@gmail.com>
2020-06-20 14:13:25 -0300

    switch from ngx-bootstrap to normal boostrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     // to-do alert, spinner, placeholderdirective
   ],
   imports: [
-    CommonModule,
-    BsDropdownModule.forRoot(),
-    ButtonsModule.forRoot(),
-    CollapseModule.forRoot()
+    CommonModule
   ],
   exports: [
-    CommonModule,
-    BsDropdownModule,
-    ButtonsModule,
-    CollapseModule
+    CommonModule
   ],
   entryComponents: [
     // to-do alert
